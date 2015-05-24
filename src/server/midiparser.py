bb0f903688d12a68149d21dfd1b58864805e87be
Shelvacu <shelvacu@gmail.com>
2015-05-24 15:48:31 +0000

    sorta working

--- src/server/midiparser.py
+++ src/server/midiparser.py
@@ -7,16 +7,16 @@ def parse_var_int(data,offset):
     nums = []
     total = 0
     while data[offset] & 0x80:
-        print(data[offset])
+        #print(data[offset])
         nums.append(data[offset] % 128)
         offset+=1
-    print(data[offset])
+    #print(data[offset])
     nums.append(data[offset] % 127)
     offset+=1
-    print(nums)
+    #print(nums)
     for num_i, num in enumerate(nums):
         total += num*((2**7)**(len(nums)-(num_i+1)))
-    print(total)
+    #print(total)
     return (total,offset)
 '''    delta_time = 0
     length = 1
@@ -44,21 +44,21 @@ def interpret_midi_data(data):
         offset+=4
         chunklength = struct.unpack(">L", data[offset:offset+4])[0]
         offset+=4
-        print("Chunktype {}, length {}".format(chunktype,chunklength))
+        #print("Chunktype {}, length {}".format(chunktype,chunklength))
         chunk = data[offset:offset+chunklength]
         chunks.append([chunktype,chunk])
         offset+=chunklength
     #Read header
     header = chunks.pop(0)
     chunkdata = header[1]
-    print(len(header[1]))
+    #print(len(header[1]))
     midi_format = struct.unpack(">H", chunkdata[0:2])
     num_tracks = struct.unpack(">H", chunkdata[2:4])
     division = struct.unpack(">H", chunkdata[4:6])
     tracks = []
     #Everything else
     for chunk in chunks:
-        print(chunk[0])
+        #print(chunk[0])
         if chunk[0] == b"MTrk":
             track = []
             tracks.append(track)
@@ -69,11 +69,11 @@ def interpret_midi_data(data):
                 delta_time,offset = parse_var_int(chunk[1],offset)
                 typebyte = chunk[1][offset]
                 offset+=1
-                print("Typebyte {},offset {}".format(typebyte, offset))
+                #print("Typebyte {},offset {}".format(typebyte, offset))
                 #running status bullshit
                 if not (typebyte & 0x80):
                     typebyte = lasttype
-                    print("CONTINUE:YES")
+                    #print("CONTINUE:YES")
                     offset-=1
                 nibble = typebyte >> 4
                 if typebyte == 0xF0 or typebyte == 0xF7:
@@ -106,22 +106,23 @@ def interpret_midi_data(data):
                     bla = 2
                     if note_on:
                         bla = 1
-                    track.append([bla,delta_time,convert.hzToMIDI(key)])
-                else:
-                    print("bad things happened. Possibly corrupt midi file")
+                    print(key)
+                    hz = 0
+                    if key > 0:
+                        hz = convert.MIDItoHz(key)
+                    track.append([bla,delta_time,hz])
                 lasttype = typebyte
     return tracks
 
-def process(data):
+def process(data,num_layers):
     tracks = interpret_midi_data(data)
-    layers_notes = [0,0,0,0,0,0]
-    layers_timing =[0,0,0,0,0,0]
-    layers = [manager.Layer(),
-              manager.Layer(),
-              manager.Layer(),
-              manager.Layer(),
-              manager.Layer(),
-              manager.Layer()]
+    layers_notes = []
+    layers_timing =[]
+    layers = []
+    for bla in range(num_layers):
+        layers_notes.append(0)
+        layers_timing.append(0)
+        layers.append(manager.Layer())
     notes_playing = {}
     normalized_tracks = []
     points = {}
@@ -132,9 +133,8 @@ def process(data):
            pos+=event[1]
            if event[0] > 0:
                points.setdefault(pos,[]).append(event)
-           else:
-               print(event)
-    s = sorted(points.items(),key=lambda x: x[0])
+    
+    s = sorted(points.items(),key=lambda x:x[0])
     '''for i,time_events in enumerate(s):
         time,events = time_events
         for event in events:
@@ -161,25 +161,25 @@ def process(data):
             #first find a layer that isn't busy
             if event[0] == 1:
                 layer_to_use = -1
-                for i in range(6):
+                for i in range(len(layers_notes)):
                     if layers_notes[i] == 0: layer_to_use = i
                 if layer_to_use == -1:
                     print("WARNING: wasn't able to alocate space for event")
                 else:
-                    layers[layer_to_use].addEvent(manager.Event(time-layers_timing[layer_to_use],event[2]))
+                    layers[layer_to_use].addEvent(manager.Event((time-layers_timing[layer_to_use])*1000,event[2]))
                     layers_notes[i] = event[2]
                     layers_timing[layer_to_use] = time
             elif event[0] == 2:
                 layer_to_use = -1
-                for i in range(6):
+                for i in range(len(layers_notes)):
                     if layers_notes [i] == event[2]:
                         layer_to_use = i
-                layers[layer_to_use].addEvent(manager.Event(time-layers_timing[layer_to_use],0))
+                layers[layer_to_use].addEvent(manager.Event((time-layers_timing[layer_to_use])*1000,0))
                 layers_notes[layer_to_use] = 0
                 layers_timing[layer_to_use] = time
 
-    print(s)
-    print(layers)
+    #print(s)
+    #print(layers)
     return layers
 '''
     num_on = 0
